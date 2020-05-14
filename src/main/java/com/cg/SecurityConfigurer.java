2fe328e9b7cd0803e6c58df4c2169d9a61c7cd21
RannvijayKumar <rannvijaykr007@gmail.com>
2020-05-14 10:51:18 +0530

    Added zuul

---
+++ src/main/java/com/cg/SecurityConfigurer.java
@@ -0,0 +1,31 @@
+package com.cg;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	JwtRequestFilter jwtRequestFilter;
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.csrf().disable()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/login/authenticate").hasAuthority("ADM")
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
+}
